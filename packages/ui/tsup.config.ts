a3c15798da2a7cc2ad014651e189cf13b5d1f137
adamsoderstrom <adamsoderstrom@me.com>
2024-09-10 12:35:39 +0200

    install `esbuild-plugin-preserve-directives`

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,3 +1,4 @@
+import { preserveDirectivesPlugin } from 'esbuild-plugin-preserve-directives'
 import { defineConfig } from "tsup";
 
 export default defineConfig((options) => ({
@@ -6,5 +7,13 @@ export default defineConfig((options) => ({
   dts: true,
   sourcemap: true,
   external: ["react"],
+  esbuildPlugins: [
+    preserveDirectivesPlugin({
+      directives: ['use client', 'use strict'],
+      // eslint-disable-next-line prefer-named-capture-group -- Is this a necessary rule?
+      include: /\.(js|ts|jsx|tsx)$/,
+      exclude: /node_modules/,
+    }),
+  ],
   ...options,
 }));
